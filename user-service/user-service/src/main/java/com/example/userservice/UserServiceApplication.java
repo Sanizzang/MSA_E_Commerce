0e2adafa112884c8fcdb61a5837f70c05ec8bdc5
Sanizzang <tksl4732@naver.com>
2023-03-30 17:57:45 +0900

    암호화 적용 및 RestTemplate, FeignClient를 이용한 마이크로서비스 간 통신 구현

--- user-service/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,15 +1,20 @@
 package com.example.userservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -24,5 +29,16 @@ public class UserServiceApplication {
 	@Bean
 	AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
 		return authConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
 	}
 }
--- user-service/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,31 +1,52 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
     UserRepository userRepository;
     BCryptPasswordEncoder passwordEncoder;
 
+    Environment env;
+    RestTemplate restTemplate;
+
+    OrderServiceClient orderServiceClient;
+
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder,
+                           Environment env,
+                           RestTemplate restTemplate,
+                           OrderServiceClient orderServiceClient) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.env = env;
+        this.restTemplate = restTemplate;
+        this.orderServiceClient = orderServiceClient;
     }
 
     @Override
@@ -59,15 +80,43 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto getUserByUserId(String userId) {
+        // 사용자 정보를 나타내는 JPA 엔티티
+        // JPA repository를 사용하여 사용자 ID를 기반으로 사용자 정보를 가져옴
         UserEntity userEntity = userRepository.findByUserId(userId);
-
+        
         if (userEntity == null)
             throw new UsernameNotFoundException("User not found");
 
+        // userEntity 객체를 UserDto 객체로 매핑하여 반환
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
+//        List<ResponseOrder> orders = new ArrayList<>();
+
+        /* Using as RestTemplate */
+        // 주문 서비스의 엔드포인트 URL을 동적으로 생성
+        // URL의 일부로 사용되는 userId는 메소드의 매개변수로 전달
+        // 환경 변수에서 order_service.url 키에 해당하는 값을 가져와 URL 템플릿의 %s 위체에 대체
+//        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+//        ResponseEntity<List<ResponseOrder>> orderListResponse =
+//                // restTemplate.exchange RestTemplate 클래스를 사용하여 HTTP 요청을 보내고, 응답을 받아옴
+//                // 이 코드에서는 주문 서비스로 요청을 보내서 해당 사용자의 주문 목록을 가져온다.
+//                // ParameterizedTypeReference<List<Response>>()는 RestTemplate에서 제공하는 제네릭 타입을 사용하는 방법으로 List<ReponseOrder> 형식의 응답을 받기 위해 사용된다.
+//                restTemplate.exchange(orderUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<ResponseOrder>>() {
+//        });
+//
+//        List<ResponseOrder> orderList = orderListResponse.getBody();
+
+        /* Using feign client */
+        /* Feign exception handling */
+        List<ResponseOrder> orderList = null;
+
+        try{
+            orderList = orderServiceClient.getOrders(userId);
+        } catch(FeignException ex) {
+            log.error(ex.getMessage());
+        }
+
+        userDto.setOrders(orderList);
 
         return userDto;
     }
